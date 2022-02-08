6f720601be1b5ce8095cd6b6c1001c964667a53b
Dilshod K <d.kobilov@mail.ru>
2022-02-09 00:05:55 +0500

    обработка ошибок

---
+++ Exception/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder();
+var app = builder.Build();
+
+app.Environment.EnvironmentName = "Production";
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler(app => app.Run(async context =>
+    {
+       context.Response.StatusCode = 500;
+       await context.Response.WriteAsync("Error 500. DivideByZeroException occurred!");
+    }));
+
+}
+app.Run(async (context) =>
+{
+    int a = 5, b = 0;
+    //int b = 0;
+    int c = a / b;
+    await context.Response.WriteAsync($"c: {c}");
+});
+
+app.Run();
